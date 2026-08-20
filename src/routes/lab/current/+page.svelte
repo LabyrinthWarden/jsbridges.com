c0216bdf42bca9e8f679560c2487a8ccde760159
Jeremiah Bridges <46149695+LabyrinthWarden@users.noreply.github.com>
2026-08-20 11:03:22 -0700

    created v4 route

--- src/routes/lab/current/+page.svelte
+++ src/routes/lab/current/+page.svelte
@@ -6,5 +6,7 @@
         <li><a href="https://getcurrentelectric.com">Current Website</a></li>
         <li><a href="/lab/current/v1">Version 1</a></li>
         <li><a href="/lab/current/v2">Version 2</a></li>
+        <li><a href="/lab/current/v3">Version 3</a></li>
+        <li><a href="/lab/current/v4">Version 4</a></li>
     </ul>
 </div>
